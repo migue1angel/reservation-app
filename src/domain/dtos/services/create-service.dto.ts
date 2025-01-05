1454b03d69128f099dff2be80d69abb641c63709
migue1angel <macf06200@gmail.com>
2025-01-04 23:09:05 -0500

    fix crete service dto

--- src/domain/dtos/services/create-service.dto.ts
+++ src/domain/dtos/services/create-service.dto.ts
@@ -3,9 +3,10 @@ export class CreateServiceDto {
     public readonly name: string,
     public readonly description: string,
     public readonly duration: number,
-    public readonly available_timetable: Date[],
-    public readonly price: number,
-    public readonly capacity: number,
+    public readonly availability: CreateAvailabilityDto,
+    public readonly pricePerPerson: number,
+    public readonly maxCapacity: number,
+    public readonly minCapacity: number,
     public readonly category: string
   ) {}
 
@@ -14,18 +15,20 @@ export class CreateServiceDto {
       name,
       description,
       duration,
-      available_timetable,
-      price,
-      capacity,
+      availability,
+      pricePerPerson,
+      maxCapacity,
+      minCapacity,
       category,
     } = object;
 
     if (!name) return ["Missing name"];
     if (!description) return ["Missing description"];
     if (!duration) return ["Missing duration"];
-    if (!available_timetable) return ["Missing available timetable"];
-    if (!price) return ["Missing price"];
-    if (!capacity) return ["Missing capacity"];
+    if (!availability) return ["Missing available timetable"];
+    if (!pricePerPerson) return ["Missing price per person"];
+    if (!maxCapacity) return ["Missing max capacity"];
+    if (!minCapacity) return ["Missing min capacity"];
     if (!category) return ["Missing category"];
 
     return [
@@ -34,10 +37,54 @@ export class CreateServiceDto {
         name,
         description,
         duration,
-        available_timetable,
-        price,
-        capacity,
+        availability,
+        pricePerPerson,
+        maxCapacity,
+        minCapacity,
         category
+      ),
+    ];
+  }
+}
+
+class CreateTimeSlotDto {
+  constructor(
+    public readonly start_time: Date,
+    public readonly end_time: Date
+  ) {}
+  static create(object: { [key: string]: any }): [string?, CreateTimeSlotDto?] {
+    const { startTime, endTime } = object;
+    if (!startTime) return ["Missing start time"];
+    if (!endTime) return ["Missing end time"];
+    return [undefined, new CreateTimeSlotDto(startTime, endTime)];
+  }
+}
+
+class CreateAvailabilityDto {
+  constructor(
+    public readonly monday?: CreateTimeSlotDto[],
+    public readonly tuesday?: CreateTimeSlotDto[],
+    public readonly wednesday?: CreateTimeSlotDto[],
+    public readonly thursday?: CreateTimeSlotDto[],
+    public readonly friday?: CreateTimeSlotDto[],
+    public readonly saturday?: CreateTimeSlotDto[],
+    public readonly sunday?: CreateTimeSlotDto[]
+  ) {}
+  static create(object: {
+    [key: string]: any;
+  }): [string?, CreateAvailabilityDto?] {
+    const { monday, tuesday, wednesday, thursday, friday, saturday, sunday } =
+      object;
+    return [
+      undefined,
+      new CreateAvailabilityDto(
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
       ),
     ];
   }
